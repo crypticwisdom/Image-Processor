e97eb63cda736d2e008e5f0bbbfbaf864afca2fe
slojar <52827399+slojar@users.noreply.github.com>
2022-01-21 12:07:40 +0100

    update

---
+++ payarena/settings/dev.py
@@ -0,0 +1,52 @@
+from .base import *
+import django_heroku
+from decouple import config
+import dj_database_url
+
+SECRET_KEY = 'b0e0f216-6bd2-4d10-9e34-9c892h273ge6-do-user-dfghjiu548642'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'payarena',
+        'USER': 'payarena',
+        'PASSWORD': 'payarena',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://localhost:80",
+    "http://localhost",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+# CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
+    'UPDATE_LAST_LOGIN': True,
+    'AUTH_HEADER_TYPES': ('Bearer', 'Token',),
+}
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
