a6685664783c1bf5d7ea2bce1930e6a71a5ed206
slojararshavin <slojararshavin@gitlab.com>
2022-10-11 11:32:19 +0100

    update account.admin

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from account.models import Profile, ForgotPasswordOTP
-# Register your models here.
-admin.site.register(Profile)
+from account.models import Profile, ForgotPasswordOTP, Address
+
+
+class AddressTabularInlineAdmin(admin.TabularInline):
+    model = Address
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "phone_number"]
+    search_fields = ["user__first_name", "user__email"]
+    list_filter = ["created_on"]
+    inlines = [AddressTabularInlineAdmin]
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(ForgotPasswordOTP)
