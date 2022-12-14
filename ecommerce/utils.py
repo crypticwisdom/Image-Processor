93cf50a4d5e4ba09419a3f1b1a732545f45dd723
slojararshavin <slojararshavin@gitlab.com>
2022-12-14 12:38:43 +0100

    update

--- ecommerce/utils.py
+++ ecommerce/utils.py
@@ -211,6 +211,7 @@ def get_shipping_rate(customer, address_id=None):
         address = Address.objects.get(customer=customer, is_primary=True)
     else:
         address = Address.objects.filter(customer=customer).first()
+
     from home.utils import log_request
     log_request("Address:", address)
 
