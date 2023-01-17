615b2d1174e233777b6c901d10374f1e441366af
crypticwisdom <crypticwisdom84@gmail.com>
2023-01-17 08:57:26 +0100

    Revamping the project to use one environment

--- image_processor/image_processor/urls.py
+++ image_processor/image_processor/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from account.views import RegisterView
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('processor/', include('processor.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
