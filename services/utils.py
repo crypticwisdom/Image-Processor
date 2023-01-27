b6ecfb96006c5b27a8b22df9eff7c0cf1905c6f4
crypticwisdom <crypticwisdom84@gmail.com>
2023-01-27 12:06:24 +0100

    Added _blank to the shopper signup email

---
+++ services/utils.py
@@ -0,0 +1,11 @@
+import requests
+from django.conf import settings
+
+BASE_URL: str = settings.
+def image_processor(client_token, block_token, images):
+    response = requests.post(url='')
+
+
+class ImageProcess:
+    def __int__(self, client_token, block_token):
+        ...
